bdd8fff5c18b82aa67654cf634328dc6e1576511
albertofermer <albertofernmer@gmail.com>
2022-11-27 09:55:26 +0100

    auto-commit:27/11/2022 a las  9:55:26,39

--- 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Práctica6_Segmentación/Primera_Parte.m
+++ 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Práctica6_Segmentación/Primera_Parte.m
@@ -14,239 +14,241 @@ nombre = "ImagenesPractica\PrimeraParte\";
 numImagenes = 3;
 
 for imagenes=1:numImagenes
-nombre_imagen = nombre + "P6_" + imagenes + ".tif";
-Imagen = imread(nombre_imagen);
-I = rgb2gray(Imagen);
+    nombre_imagen = nombre + "P6_" + imagenes + ".tif";
+    Imagen = imread(nombre_imagen);
+    I = rgb2gray(Imagen);
 
-%-------------------------------------------------------------------------%
-% 2. A partir de I, generar una imagen de bordes (Ib) aplicando un detector
-% de bordes verticales dado por una máscara correspondiente de Sobel.
-% Considerando como umbral el 30% del valor máximo de la matriz magnitud
-% del vector gradiente obtenida por el detector.
-%-------------------------------------------------------------------------%
+    %-------------------------------------------------------------------------%
+    % 2. A partir de I, generar una imagen de bordes (Ib) aplicando un detector
+    % de bordes verticales dado por una máscara correspondiente de Sobel.
+    % Considerando como umbral el 30% del valor máximo de la matriz magnitud
+    % del vector gradiente obtenida por el detector.
+    %-------------------------------------------------------------------------%
 
-% Máscaras de Sobel
-Hx_sobel = [-1 0 1; -2 0 2; -1 0 1]; 
-Hy_sobel = [-1, 2 -1; 0 0 0; 1 2 1];
-[Gx,Gy,modG] = funcion_calcula_gradiente(I,Hx_sobel, Hy_sobel);
+    % Máscaras de Sobel
+    Hx_sobel = [-1 0 1; -2 0 2; -1 0 1];
+    Hy_sobel = [-1, 2 -1; 0 0 0; 1 2 1];
+    [Gx,Gy,modG] = funcion_calcula_gradiente(I,Hx_sobel, Hy_sobel);
 
-%-------------------------------------------------------------------------%
-% Gx contiene la magnitud de los píxeles de borde verticales, por lo que
-% deberemos binarizar dicha imagen.
-%-------------------------------------------------------------------------%
+    %-------------------------------------------------------------------------%
+    % Gx contiene la magnitud de los píxeles de borde verticales, por lo que
+    % deberemos binarizar dicha imagen.
+    %-------------------------------------------------------------------------%
 
-magnitud_Gx = abs(Gx);
-Ib = uint8(magnitud_Gx) > 0.3*max(magnitud_Gx(:));
+    magnitud_Gx = abs(Gx);
+    Ib = uint8(magnitud_Gx) > 0.3*max(magnitud_Gx(:));
 
-%-------------------------------------------------------------------------%
-% 3. Aplica a Ib la Transformada de Hough para detectar líneas rectas
-% utilizando la función de matlab hough.
-%-------------------------------------------------------------------------%
-[H,theta, rho] = hough(Ib, 'Theta', -90:89);
+    %-------------------------------------------------------------------------%
+    % 3. Aplica a Ib la Transformada de Hough para detectar líneas rectas
+    % utilizando la función de matlab hough.
+    %-------------------------------------------------------------------------%
+    [H,theta, rho] = hough(Ib, 'Theta', -90:89);
 
-% ======================== PREGUNTAS =====================================%
-% ¿Qué representan theta y rho?
+    % ======================== PREGUNTAS =====================================%
+    % ¿Qué representan theta y rho?
 
-% RESP:
-% theta:ángulo de la recta perpendicular que va desde el punto de 
-% coordenadas hasta la recta.  
-% rho: distancia del origen de coordenadas a la recta
+    % RESP:
+    % theta:ángulo de la recta perpendicular que va desde el punto de
+    % coordenadas hasta la recta.
+    % rho: distancia del origen de coordenadas a la recta
 
-% ¿Cuál es el significado de los valores almacenados en H?
-%
-% RESP:
-% El número de puntos alineados que se encuentran en cada recta.
+    % ¿Cuál es el significado de los valores almacenados en H?
+    %
+    % RESP:
+    % El número de puntos alineados que se encuentran en cada recta.
 
-% ¿Cómo es la discretización que se realiza del espacio de parámetros en
-% esta configuración por defecto?
-%
-% RESP:
-% Angulo desde -90 hasta 89
-% La distancia: desde -taumax hasta +taumax
-%               donde taumax = round(sqrt(M^2+N^2))
-% ========================================================================%
+    % ¿Cómo es la discretización que se realiza del espacio de parámetros en
+    % esta configuración por defecto?
+    %
+    % RESP:
+    % Angulo desde -90 hasta 89
+    % La distancia: desde -taumax hasta +taumax
+    %               donde taumax = round(sqrt(M^2+N^2))
+    % ========================================================================%
 
-%-------------------------------------------------------------------------%
-% Ayudándote de la función de matlab find, escribe la ecuacion de la recta
-% que pasa por más puntos en la imagen binaria Ib.
-%-------------------------------------------------------------------------%
+    %-------------------------------------------------------------------------%
+    % Ayudándote de la función de matlab find, escribe la ecuacion de la recta
+    % que pasa por más puntos en la imagen binaria Ib.
+    %-------------------------------------------------------------------------%
 
-% Buscamos la fila y la columna donde H es máxima.
-[fila,columna] = find(H == max(H(:)));
+    % Buscamos la fila y la columna donde H es máxima.
+    [fila,columna] = find(H == max(H(:)));
 
-% Nos dará la fila en el vector de distancias y la columna en el vector de
-% ángulos, por lo que deberemos encontrar el valor correspondiente a esas
-% posiciones.
+    % Nos dará la fila en el vector de distancias y la columna en el vector de
+    % ángulos, por lo que deberemos encontrar el valor correspondiente a esas
+    % posiciones.
 
-rho_recta = rho(fila); theta_recta = theta(columna);
+    rho_recta = rho(fila); theta_recta = theta(columna);
 
-% Discretizamos el espacio de coordenadas para representar la recta.
-[N,M] = size(I);
-% Utilizamos M porque como mucho la recta llegará a esa posición
-[X,~] = meshgrid(1:M);
+    % Discretizamos el espacio de coordenadas para representar la recta.
+    [N,M] = size(I);
+    % Utilizamos M porque como mucho la recta llegará a esa posición
+    [X,~] = meshgrid(1:M);
 
-% Calculamos la Y correspondiente a cada X
-Y = (rho_recta - X*cosd(theta_recta) ) / sind(theta_recta);
+    % Calculamos la Y correspondiente a cada X
+    Y = (rho_recta - X*cosd(theta_recta) ) / sind(theta_recta);
 
-% Mostramos la recta más probable.
-%imshow(Ib), hold on
-%plot(X,Y,'.r');
+    % Mostramos la recta más probable.
+    %imshow(Ib), hold on
+    %plot(X,Y,'.r');
 
-% Escribimos la ecuación de la recta que pasa por más puntos.
-disp("La recta que pasa por más puntos es: " + (rho_recta) + " = " + "xcos(" + theta_recta + ") + " + "ysen(" + (theta_recta) + ")");
+    % Escribimos la ecuación de la recta que pasa por más puntos.
+    disp("La recta que pasa por más puntos es: " + (rho_recta) + " = " + "xcos(" + theta_recta + ") + " + "ysen(" + (theta_recta) + ")");
 
-%-------------------------------------------------------------------------%
-% 4. Encuentra los parámetros representativos de las 5 rectas más votadas.
-% Para ello aplica la funcion de matlab houghpeaks
-%-------------------------------------------------------------------------%
+    %-------------------------------------------------------------------------%
+    % 4. Encuentra los parámetros representativos de las 5 rectas más votadas.
+    % Para ello aplica la funcion de matlab houghpeaks
+    %-------------------------------------------------------------------------%
 
-NumRectas = 5; Umbral = ceil(0.3*max(H(:)));
-P = houghpeaks(H,NumRectas,'threshold', Umbral);
+    NumRectas = 5; Umbral = ceil(0.3*max(H(:)));
+    P = houghpeaks(H,NumRectas,'threshold', Umbral);
 
-% Sacamos los valores de los parámetros de cada recta:
+    % Sacamos los valores de los parámetros de cada recta:
 
-rho_rectas = rho(P(:,1)); % Distancias
+    rho_rectas = rho(P(:,1)); % Distancias
 
-theta_rectas = theta(P(:,2)); % Ángulos
+    theta_rectas = theta(P(:,2)); % Ángulos
 
-% Representamos las rectas sobre la imagen binaria.
-figure, imshow(Ib), hold on
-for j=1:length(rho_rectas)
-    Y = (rho_rectas(j) - X*cosd(theta_rectas(j)) ) ./ sind(theta_rectas(j));
+    % Representamos las rectas sobre la imagen binaria.
+    figure, imshow(Ib), title("5 rectas más probables"),hold on
+    for j=1:length(rho_rectas)
+        Y = (rho_rectas(j) - X*cosd(theta_rectas(j)) ) ./ sind(theta_rectas(j));
 
-    % No es necesario cambiar el orden de las coordenadas porque figure ya
-    % tiene los ejes de coordenadas de la imagen (imshow).
+        % No es necesario cambiar el orden de las coordenadas porque figure ya
+        % tiene los ejes de coordenadas de la imagen (imshow).
 
-    plot(X,Y,'.r');
-end
-
-
-% ======================== PREGUNTAS =====================================%
-% ¿Qué información contiene el parámetro de salida P?
-%
-% RESP:
-% P contiene la fila y la columna de H donde se encuentran las NumRectas
-% más votadas.
-
-% ¿Qué significado tiene la inclusión del parámetro de entrada Umbral en la
-% función?
-%
-% RESP:
-% El parámetro Umbral se utiliza para excluir las posibles rectas que
-% tengan un valor de H menor a dicho umbral, es decir, excluye las rectas
-% que pasen por un número de puntos menor al umbral.
-
-% ¿Qué efecto tiene en los resultados finales fijar el umbral con un valor ceil(0.5*max(H(:))?
-%
-% RESP:
-% Que no considerará rectas aquellas celdas en las que haya un número de
-% puntos inferior al de la mitad de la recta más votada.
-%=========================================================================%
-
-%-------------------------------------------------------------------------%
-% 5. Muestra los segmentos de puntos de Ib que incluyen a las 5 rectas
-% detectadas.
-%-------------------------------------------------------------------------%
-
-%************************ CÓDIGO DEL ENUNCIADO ***************************%
-lines = houghlines(Ib,theta,rho,P,'FillGap',5,'MinLength',7);
-figure, imshow(Ib), hold on
-max_len = 0; 
-for k = 1:length(lines)
-    xy = [lines(k).point1; 
-    lines(k).point2];
-    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-% Plot beginnings and ends of lines 
-    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow'); 
-    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-% Determine the endpoints of the longest line segment 
-    len = norm(lines(k).point1 - lines(k).point2);
-    if ( len > max_len)
-        max_len = len; 
-        xy_long = xy;
+        plot(X,Y,'.r');
     end
-end
-% highlight the longest line segment
-plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
-%*************************************************************************%
 
-% ======================== PREGUNTAS =====================================%
-% ¿Qué información contiene la variable lines?
-%
-% RESP:
-% Contiene una estructura por cada línea que hay en la imagen:
-%   point1 y point2: son dos puntos contenidos en la recta
-%   theta: es el ángulo que tiene la recta que va desde el origen a la 
-%       recta de la imagen perpendicularmente
-%   rho: es la distancia hasta origen de coordenadas.
-%
-% En definitiva, contiene dos puntos de cada recta junto con los parámetros
-% de dicha recta.
-%
-%
-% ¿Qué significado tienen las opciones elegidas en la llamada de la función
-% ('FillGap',5,'MinLength',7)
 
-% 'FillGap',5 : Cuando encuentra dos líneas asociadas a la misma
-% transoformada de Hough separadas por menos de 5 píxeles, las considera la
-% misma línea y las fusiona en un solo segmento.
-% 
-% 'MinLength', 7:  Los segmentos fusionados menores de 7 píxeles se
-% descartan.
-% ========================================================================%
+    % ======================== PREGUNTAS =====================================%
+    % ¿Qué información contiene el parámetro de salida P?
+    %
+    % RESP:
+    % P contiene la fila y la columna de H donde se encuentran las NumRectas
+    % más votadas.
 
-%-------------------------------------------------------------------------%
-% 6. A partir de la información obtenida en los pasos anteriores, realiza
-% la segmentación de la carretera.
-%-------------------------------------------------------------------------%
+    % ¿Qué significado tiene la inclusión del parámetro de entrada Umbral en la
+    % función?
+    %
+    % RESP:
+    % El parámetro Umbral se utiliza para excluir las posibles rectas que
+    % tengan un valor de H menor a dicho umbral, es decir, excluye las rectas
+    % que pasen por un número de puntos menor al umbral.
 
-% 6.1 Sobre una imagen binaria inicializada a '1' de las mismas dimensiones
-% que la imagen de intensidad original, asigna un valor '0' a todos los
-% píxeles presentes en las rectas detectadas en P.
+    % ¿Qué efecto tiene en los resultados finales fijar el umbral con un valor ceil(0.5*max(H(:))?
+    %
+    % RESP:
+    % Que no considerará rectas aquellas celdas en las que haya un número de
+    % puntos inferior al de la mitad de la recta más votada.
+    %=========================================================================%
 
-Ibinaria = ones(N,M); % Imagen binaria de las mismas dimensiones que I.
+    %-------------------------------------------------------------------------%
+    % 5. Muestra los segmentos de puntos de Ib que incluyen a las 5 rectas
+    % detectadas.
+    %-------------------------------------------------------------------------%
 
-for j=1:length(P) % Por cada una de las rectas de P:
-    % Se calculan sus puntos.
-    Y = (rho_rectas(j) - X*cosd(theta_rectas(j)) ) / sind(theta_rectas(j));
-    
-    % Nos quedamos con los puntos que nos interesan (positivos y menores
-    % que N).
-
-    YoI = Y(Y>0 & Y<=N);
-    XoI = X(Y>0 & Y<=N);
-
-    for i=1:length(XoI) % Por cada valor de X
-        % Ponemos a 0 los píxeles correspondientes.
-        Ibinaria(ceil(YoI(i)),ceil(XoI(i))) = 0;
+    %************************ CÓDIGO DEL ENUNCIADO ***************************%
+    lines = houghlines(Ib,theta,rho,P,'FillGap',5,'MinLength',7);
+    figure, imshow(Ib), title("Puntos de las rectas más probables"), hold on
+    max_len = 0;
+    for k = 1:length(lines)
+        xy = [lines(k).point1;
+            lines(k).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+        % Plot beginnings and ends of lines
+        plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+        plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+        % Determine the endpoints of the longest line segment
+        len = norm(lines(k).point1 - lines(k).point2);
+        if ( len > max_len)
+            max_len = len;
+            xy_long = xy;
+        end
     end
-end
+    % highlight the longest line segment
+    plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
+    %*************************************************************************%
+
+    % ======================== PREGUNTAS =====================================%
+    % ¿Qué información contiene la variable lines?
+    %
+    % RESP:
+    % Contiene una estructura por cada línea que hay en la imagen:
+    %   point1 y point2: son dos puntos contenidos en la recta
+    %   theta: es el ángulo que tiene la recta que va desde el origen a la
+    %       recta de la imagen perpendicularmente
+    %   rho: es la distancia hasta origen de coordenadas.
+    %
+    % En definitiva, contiene dos puntos de cada recta junto con los parámetros
+    % de dicha recta.
+    %
+    %
+    % ¿Qué significado tienen las opciones elegidas en la llamada de la función
+    % ('FillGap',5,'MinLength',7)
+
+    % 'FillGap',5 : Cuando encuentra dos líneas asociadas a la misma
+    % transoformada de Hough separadas por menos de 5 píxeles, las considera la
+    % misma línea y las fusiona en un solo segmento.
+    %
+    % 'MinLength', 7:  Los segmentos fusionados menores de 7 píxeles se
+    % descartan.
+    % ========================================================================%
+
+    %-------------------------------------------------------------------------%
+    % 6. A partir de la información obtenida en los pasos anteriores, realiza
+    % la segmentación de la carretera.
+    %-------------------------------------------------------------------------%
+
+    % 6.1 Sobre una imagen binaria inicializada a '1' de las mismas dimensiones
+    % que la imagen de intensidad original, asigna un valor '0' a todos los
+    % píxeles presentes en las rectas detectadas en P.
+
+    Ibinaria = ones(N,M); % Imagen binaria de las mismas dimensiones que I.
+
+    for j=1:length(P) % Por cada una de las rectas de P:
+        % Se calculan sus puntos.
+        Y = (rho_rectas(j) - X*cosd(theta_rectas(j)) ) / sind(theta_rectas(j));
+
+        % Nos quedamos con los puntos que nos interesan (positivos y menores
+        % que N).
+
+        YoI = Y(Y>0 & Y<=N);
+        XoI = X(Y>0 & Y<=N);
+
+        for i=1:length(XoI) % Por cada valor de X
+            % Ponemos a 0 los píxeles correspondientes.
+            Ibinaria(ceil(YoI(i)),ceil(XoI(i))) = 0;
+        end
+    end
 
 
-% 6.2 Aplicar un filtro de mínimos 3x3 a la imagen binaria para unir los
-% puntos de las líneas detectadas y delimitar regiones candidatas a ser la
-% zona principal de la carretera.
+    % 6.2 Aplicar un filtro de mínimos 3x3 a la imagen binaria para unir los
+    % puntos de las líneas detectadas y delimitar regiones candidatas a ser la
+    % zona principal de la carretera.
 
-Ibinaria_filt = ordfilt2(Ibinaria,1,ones(3));
-figure, imshow(Ibinaria_filt)
+    Ibinaria_filt = ordfilt2(Ibinaria,1,ones(3));
+    figure, imshow(Ibinaria_filt), title("Imagen con filtro de mínimos 3x3")
 
-% 6.3 Genera y visualiza la imagen binaria que representa la segmentación
-% de la carretera asumiendo que es la región que contiene al píxel central
-% de la imagen.
+    % 6.3 Genera y visualiza la imagen binaria que representa la segmentación
+    % de la carretera asumiendo que es la región que contiene al píxel central
+    % de la imagen.
 
-% Primero etiquetamos las zonas candidatas a ser la zona principal de la
-% carretera.
-Ietiq = bwlabel(Ibinaria_filt);
+    % Primero etiquetamos las zonas candidatas a ser la zona principal de la
+    % carretera.
+    Ietiq = bwlabel(Ibinaria_filt);
 
-% Escogemos la etiqueta que corresponde con el píxel central de la imagen.
-etiq = Ietiq(round(N/2),round(M/2));
+    % Escogemos la etiqueta que corresponde con el píxel central de la imagen.
+    etiq = Ietiq(round(N/2),round(M/2));
 
-% La imagen de interés serán aquellos píxeles que tengan como etiqueta la
-% misma que la del píxel central.
-IOI = (Ietiq == etiq);
+    % La imagen de interés serán aquellos píxeles que tengan como etiqueta la
+    % misma que la del píxel central.
+    IOI = (Ietiq == etiq);
 
-% Visualizamos la zona de interés en la imagen original.
-figure, imshow(Imagen .* uint8(IOI))
+    % Visualizamos la zona de interés en la imagen original.
+    figure, imshow(Imagen .* uint8(IOI)), title("Imagen Segmentada")
+    pause
+    close all
 
 end
 
--- 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Práctica6_Segmentación/Segunda_Parte_senales_azules.m
+++ 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Práctica6_Segmentación/Segunda_Parte_senales_azules.m
@@ -1,44 +1,51 @@
 %-------------------------------------------------------------------------%
 %%                      SEGUNDA  PARTE                                   %%
-%               Segmentación de señales de tráfico (azules)               %
+%               Segmentación de señales de tráfico (azules)                %
 %-------------------------------------------------------------------------%
 addpath('Funciones Necesarias\')
 addpath('Funciones_THCircular\')
+
+%-------------------------------------------------------------------------%
+% 8. Lee la imagen, genera y visualiza una imagen binaria (Ib) de puntos
+% "rojos" de la imagen en color candidatos a pertenecer al contorno de la
+% región que se pretende segmentar. Para ello utiliza un metodo de
+% umbralización global.
+%-------------------------------------------------------------------------%
+
 nombre = "ImagenesPractica\SegundaParte\Signal4";
-
 numImagenes = 2;
-
 for imagenes=1:numImagenes
 
     nombre_imagen = nombre + "_" + num2str(imagenes) + ".tif";
     Imagen = imread(nombre_imagen);
 
-    % 1. Generar y visualizar una imagen binaria de puntos rojos de la imagen
-    % de color candidatos a pertenecer al contorno de la región que se pretende
-    % segmentar. Utilizar un método de umbralización global, tomando como
-    % umbrales de referencia para cada componente de color:
-
     factor = 0.35;
+    % Sacamos las componentes de color de la imagen.
     R = Imagen(:,:,1); G = Imagen(:,:,2); B = Imagen(:,:,3);
 
+    % Sacamos el mínimo y el máximo de cada componente de color.
     Rmin = min(R(:)); Rmax = max(R(:));
     Gmin = min(G(:)); Gmax = max(G(:));
     Bmin = min(B(:)); Bmax = max(B(:));
 
+    % Calculamos el umbral de cada componente de color.
     UmbralRojo = factor*(Rmin+Rmax);
     UmbralVerde = factor*(Gmin+Gmax);
     UmbralAzul = factor*(Bmin+Bmax);
 
+    % Buscamos los píxeles rojos, por lo que las imágenes binarias son:
     Rbin = R<UmbralRojo;
     Gbin = G<UmbralVerde;
     Bbin = B>UmbralAzul;
 
-    Ib = and(Rbin,and(Gbin,Bbin)); % Multiplicamos las tres imagenes para segmentar las tres componentes.
-    figure,
-    imshow(Ib)
+    % Multiplicamos las tres imagenes para segmentar las tres componentes.
+    Ib = and(Rbin,and(Gbin,Bbin));
+    figure, imshow(Ib)
 
-    % 2. Aplica a Ib la transformada de Hough para detectar contornos
+    %-----------------------------------------------------------------%
+    % 9. Aplica a Ib la transformada de Hough para detectar contornos
     % circulares utilizando la función circle_hough.
+    %-----------------------------------------------------------------%
 
     radii = 5:2:35; % Radios posibles de las circunferencias buscadas
 
@@ -50,66 +57,112 @@ for imagenes=1:numImagenes
     % La opción 'same' hace que los centros de la circunferencia sean centros
     % pertenecientes a píxeles de la imagen.
 
-    % Interpretación de las dimensiones de H:
+    % ====================== PREGUNTAS ===============================%
+    % Interpreta las dimensiones de H. ¿Cuál es el significado de los
+    % valores almacenados en la matriz?
     %
+    % RESP:
     % H tiene esas dimensiones (360x480x16) porque 360x480 son los píxeles de
     % la imagen y, por tanto, cada píxel se convierte en un posible centro de
     % cada círculo. La tercera dimensión (16) corresponde con cada uno de los
     % radios que le hemos pasado a la función.
     %
-
+    % Los valores almacenados en H son, por tanto, todos los puntos por
+    % los que pasa una circunferencia de centro NxM y de radio R.
+    %
+    %
     % Calcula manualmente H(180,90,6) e interpreta el resultado.
     H(180,90,6); % = 0
-    % En el píxel 180,90 no hay una circunferencia de radio 6.
-
+    %
+    % RESP:
+    % En el píxel (180,90) no hay una circunferencia de radio 6.
+    %
     % Ayudándote de la funcion find escribe la ecuacion de la circunferencia
     % que pasa por más puntos en Ib. Especificando su radio y coordenadas del
-    % centro.
+    % centro. ¿Cuántos puntos de Ib contiene esta cirunferencia?
 
+    % Inicializamos la matriz de resultados.
     matCirc = [];
-    for r = 1:length(radii)
+
+
+    for r = 1:length(radii) % por cada uno de los radios discretizados:
+
+        % Sacamos la matriz H correspondiente al radio.
         Hr = H(:,:,r);
+
+        % Sacamos la fila y la columna del máximo.
         [fil,col] = find(H(:,:,r) == max(Hr(:)));
+
+        % Lo añadimos a la matriz de resultados.
         matCirc = [matCirc; r, radii(r), fil(1), col(1), H(fil(1),col(1),r)];
     end
+
+
     % La circunferencia que contiene más puntos será:
     [~,pos] = max(matCirc(:,5));
+    % Por tanto, la circunferencia más probable:
     circ_mas_probable = matCirc(pos,:);
 
     % Escribir la ecuación de la circunferencia:
+    % r^2 = (x-a)^2 + (y-b)^2
     disp(num2str(circ_mas_probable(2))+"^2 = (x - " + num2str(circ_mas_probable(3)) + ")^2 + ( y - " + num2str(circ_mas_probable(4)) + ")^2")
     disp(" La circunferencia contiene " + num2str(circ_mas_probable(end)) + " puntos.")
+    % =================================================================%
 
-    % Encuentra los parámetros representativos de la circunferencia más votada
-    % aplicando la funcion circle_houghpeaks con la siguiente configuracion:
+    %-----------------------------------------------------------------%
+    % 10. Encuentra los parámetros representativos de la circunferencia
+    % más votada aplicando la funcion circle_houghpeaks con la
+    % siguiente configuracion:
+    %-----------------------------------------------------------------%
+
     P = circle_houghpeaks(H,radii,'npeaks',1);
 
+    % ====================== PREGUNTAS ============================== %
+    % ¿Qué información tiene el parámetro de salida P?
+    %
+    % RESP:
     % El parámetro P contiene el radio, las coordenadas del centro y el
     % número de puntos por los que pasa la circunferencia.
+    % ================================================================%
 
+    %-----------------------------------------------------------------%
+    % 11. A partir de la información contenida en P, genera una imagen
+    % binaria de las mismas dimensiones que las matrices que componen
+    % la imagen original que especifique los píxeles correspondientes
+    % a la circunferencia detectada.
+    % Para ello, usa la función circlepoints.
+    %-----------------------------------------------------------------%
 
+    % Generar una imagen binaria de las mismas dimensiones que la
+    % imagen binaria original.
+    Ib_circunf = false(size(R));
 
-    % 11. A partir de la información contenida en P, genera una imagen binaria
-    % de las mismas dimensiones que las matrices que componen la imagen
-    % original que especifique los píxeles correspondientes a la circunferencia
-    % detectada. Para ello, usa la función circlepoints.
-
-    [N,M] = size(R);
-    Ib_circunf = zeros(N,M);
-
+    % Obtenemos los puntos de la circunferencia más probable.
     [x,y] = circlepoints(P(3));
+
+    % Por cada punto de la circunferencia
     for i = 1:length(x)
-        Ib_circunf(y(i)+P(2),x(i)+P(1)) = 1;
+        % Le sumamos la coordenada del centro para centrar la
+        % circunferencia en esas coordenadas. Porque la función
+        % circlepoints da los puntos centrados en el (0,0).
+        Ib_circunf(y(i)+P(2),x(i)+P(1)) = true;
     end
-    figure,
-    imshow(Ib_circunf)
 
-    % 12. Genera y visualiza la imagen binaria que representa la segmentación
-    % de la señal de tráfico Ib_circle utilizando la funcion imfill:
+    figure, imshow(Ib_circunf)
 
+    %-----------------------------------------------------------------%
+    % 12. Genera y visualiza la imagen binaria que representa la
+    % segmentación de la señal de tráfico Ib_circle utilizando la
+    % funcion imfill:
+    %-----------------------------------------------------------------%
     Ib_circulo = imfill(Ib_circunf,'holes');
 
+    % Multiplico la imagen original por la imagen binaria. De esta
+    % forma podemos mostrar la zona de la imagen que nos interesa.
     figure, imshow(Imagen .* uint8(Ib_circulo))
     pause;
     close all
 end
+rmpath('Funciones Necesarias\')
+rmpath('Funciones_THCircular\')
+clear all
