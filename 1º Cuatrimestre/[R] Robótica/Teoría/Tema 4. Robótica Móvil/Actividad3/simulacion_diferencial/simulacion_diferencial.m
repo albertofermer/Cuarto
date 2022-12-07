834cd664bd317aca65f315cfb86e700fd209249b
albertofermer <albertofernmer@gmail.com>
2022-12-07 13:33:42 +0100

    auto-commit:07/12/2022 a las 13:33:42,38

--- 1º Cuatrimestre/[R] Robótica/Teoría/Tema 4. Robótica Móvil/Actividad3/simulacion_diferencial/simulacion_diferencial.m
+++ 1º Cuatrimestre/[R] Robótica/Teoría/Tema 4. Robótica Móvil/Actividad3/simulacion_diferencial/simulacion_diferencial.m
@@ -21,12 +21,12 @@ l=3.5; %distancia entre rudas delanteras y traseras, tambien definido en modelo
 radio_rueda=1;
 
 %Condiciones iniciales
-pose0=[20; 20; 0];
+pose0=[0; 0; 0];
 
 t0=0;
 
 %final de la simulación
-tf=30;
+tf=60;
 
 %paso de integracion
 h=0.1;
@@ -37,10 +37,12 @@ k=0;
 
 %inicialización valores iniciales
 pose(:,k+1)=pose0;
-
+punto = [30,50];
 t(k+1)=t0;
 grid on
-while (t0+h*k) < tf
+while ((t0+h*k) < tf)
+    
+    %punto(1)==pose(1,k) && (punto(2) == pose(2,k));
     %actualización
     k=k+1;
 
@@ -48,32 +50,43 @@ while (t0+h*k) < tf
     %valores de los parámetros de control
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    orden_minimo = minima_distancia(camino,[pose(1,k),pose(2,k)]);
+    punto = camino(orden_minimo+25,:);
+    
+    % --- Controlador de Alto Nivel ----
 
-    R=20;
 
-    rho=1/R;
+    delta = (pose(1,k) - punto(1))*sin(pose(3,k))-(pose(2,k) - punto(2))*cos(pose(3,k));
+    LH = sqrt( (pose(1,k) - punto(1))^2 + (pose(2,k) - punto(2))^2 );
+    rho=(2*delta)/(LH^2);
+    kv = 1;
     velocidad_lineal = 10;
+    %velocidad_lineal = kv*LH;
+    
+    if (velocidad_lineal > 10)
+        velocidad_lineal = 10;
+    elseif (velocidad_lineal < -10)
+         velocidad_lineal = -10;
+    end
+    %velocidad_lineal = 10;
+    % --------------------------------- 
+    %           Modelo Inverso
+    % ---------------------------------
+
     velocidad_derecha=(velocidad_lineal/radio_rueda)*(1+l*rho);
     velocidad_izquierda=(velocidad_lineal/radio_rueda)*(1-l*rho);
 
+    %phi=atan(rho*l);
 
-    phi=atan(rho*l);
+    %volante=-0.1416;
 
-    volante=-0.1416;
-
-    volante=phi;
-
-    velocidad=2;
-
-
+    %volante=phi;
 
     conduccion=[velocidad_derecha velocidad_izquierda];
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     %para representar el punto onjetivo sobre la trayectoria
-
-    punto=[30 30];
 
 
     %metodo de integración ruge-kuta
--- 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Trabajo_SegmentacionReconocimientoCaracteresMatriculas/PrimeraFase_SegmentacionCaracteres.m
+++ 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Trabajo_SegmentacionReconocimientoCaracteresMatriculas/PrimeraFase_SegmentacionCaracteres.m
@@ -12,25 +12,30 @@ for imagen=1:numImagenes
     I = imread(nombre);
     R = I(:,:,1);
 
-    % Suavizamos la imagen con un filtro gaussiano para limpiarla.
-    % Se utiliza el padding='replicate' para evitar falsos bordes alrededor de
-    % la imagen.
-    Rsuavizada = imfilter(R,fspecial('gaussian',W,sigma),'replicate');
-    % Umbralizacion Local para corregir defectos de iluminacion
+%     % Suavizamos la imagen con un filtro gaussiano para limpiarla.
+%     % Se utiliza el padding='replicate' para evitar falsos bordes alrededor de
+%     % la imagen.
+     Rsuavizada = imfilter(R,fspecial('gaussian',W,sigma),'replicate');
 
-    Ifilt = (funcion_umbralizacion_local(Rsuavizada,ones(W),'zeros'));
-    figure('Name',"Imagen Umbralizacion local  " + imagen)
-    Ifilt3 = bwareaopen(Ifilt,100); %% Justificar la eleccion del umbral
-    imshow(Ifilt3)
+     %% OTSU - No se debe usar
+%     umbral = graythresh(Rsuavizada);
+%     figure, imshow(Rsuavizada<255*umbral)
 
-    [Ietiq] = bwlabel(Ifilt3);
-    imshow(Ietiq == 10)
+    %% OPCION-1: MEDIA-C. Umbralizacion Local para corregir defectos de iluminacion 
+     Ifilt = (funcion_umbralizacion_local(Rsuavizada,ones(25),'zeros'));
+     figure('Name',"Imagen Umbralizacion local  " + imagen)
+     Ifilt3 = ordfilt2(Ifilt,8*8,ones(8),'zeros'); %% Justificar la eleccion del umbral
+     Ifilt4 = ordfilt2(Ifilt3,1,ones(8),'zeros');
+     imshow(Ifilt4)
+
+    %% OPCION-2: RESTA DE IMAGEN DE FONDO - IMAGEN RUIDOSA.
+
+%     [Ietiq] = bwlabel(Ifilt3);
+%     imshow(Ietiq == 10)
 
     % Me quedo con las etiquetas que haya en la línea central de la imagen.
 
 %     Word = 5;
-%     Ifiltrada = ordfilt2((Idesv),Word*Word,ones(Word));
-%     Ifiltrada = ordfilt2(Ifiltrada,1,ones(Word));
 %     figure('Name',"Imagen Final"), imshow(Ifiltrada ~= 0)
 
     % Mostramos la imagen segmentada con el umbral de otsu.
