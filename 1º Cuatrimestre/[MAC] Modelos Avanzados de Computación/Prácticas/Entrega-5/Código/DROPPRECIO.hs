43328807511ba256df94df585728ddacc12f0435
albertofermer <albertofernmer@gmail.com>
2022-12-12 08:35:39 +0100

    auto-commit:12/12/2022 a las  8:35:38,81

--- 1º Cuatrimestre/[MAC] Modelos Avanzados de Computación/Prácticas/Entrega-5/Código/DROPPRECIO.hs
+++ 1º Cuatrimestre/[MAC] Modelos Avanzados de Computación/Prácticas/Entrega-5/Código/DROPPRECIO.hs
@@ -2,29 +2,44 @@
 ------------ Declaración de los tipos de dato -----------------
 type ListaIngredientes = [String]
 type Precio = Float
-data Pizza = Pizza { ingredientes :: ListaIngredientes, precio::Precio }deriving(Show)
+data Pizza = Pizza { ingredientes :: ListaIngredientes, precio::Precio }deriving(Eq,Show)
 --------------------------------------------------------------
 
---- Definición de las pizzas ------------------------------------------------------
+--- Definición de las pizzas --------------------------------------------------------
 margarita = Pizza {ingredientes = ["Tomate","Queso","Jamon York"], precio=10}
-cuatroquesos = Pizza {ingredientes = ["Tomate","Roquefort","Gouda","Emmental","Mozzarella"], precio=12.5}
-barbacoa = Pizza {ingredientes = ["Tomate", "Salsa BBQ", "Bacon", "Cebolla", "Ternera"], precio=14}
-marinera = Pizza {ingredientes = ["Tomate", "Atun", "Anchoas","Aceitunas Negras"], precio=12.5}
-campestre = Pizza {ingredientes = ["Tomate", "Champignones", "Pimiento", "Cebolla"], precio=12.5}
-hawaiana = Pizza {ingredientes = ["Tomate", "Pina", "Maiz", "Jamon York"], precio= 13}
--------------------------------------------------------------------------------------
-
---- Creación de la Lista de Pizzas --------------------------------------------------
-listaPizzas = [margarita,cuatroquesos,barbacoa,marinera,campestre,hawaiana]
--------------------------------------------------------------------------------------
-
---- Función DROPPRECIO --------------------------------------------------------------
-dropPrecio :: Float -> [Pizza]
-dropPrecio x = dropPrecioRec listaPizzas x
-
-dropPrecioRec :: [Pizza] -> Float -> [Pizza]
-dropPrecioRec [] _ = []
-dropPrecioRec (cab:rest) x
-	| (precio cab) > x = [cab] ++ dropPrecioRec rest x
-	| otherwise = dropPrecioRec rest x
+cuatroquesos = Pizza {ingredientes = ["Tomate","Roquefort","Gouda","Emmental","Mozzarella"], precio=12}
+barbacoa = Pizza {ingredientes = ["Tomate", "Salsa BBQ", "Bacon", "Cebolla", "Ternera"], precio=16}
+marinera = Pizza {ingredientes = ["Tomate", "Atun", "Anchoas","Aceitunas Negras"], precio=13}
+campestre = Pizza {ingredientes = ["Tomate", "Champignones", "Pimiento", "Cebolla"], precio=9}
+hawaiana = Pizza {ingredientes = ["Tomate", "Pina", "Maiz", "Jamon York"], precio= 12}
 ---------------------------------------------------------------------------------------
+
+--- Creación de la Lista de Pizzas ----------------------------------------------------
+listaPizzas = [margarita,cuatroquesos,barbacoa,marinera,campestre,hawaiana]
+---------------------------------------------------------------------------------------
+
+--- Obtiene la lista de pizzas con un precio superior a X
+precioSuperiorA :: Precio -> [Pizza] -> [Pizza]
+precioSuperiorA _ [] = []
+precioSuperiorA x (cab:rest)
+	| precio(cab) >= x = [cab]++(precioSuperiorA x rest)
+	| otherwise = (precioSuperiorA x rest)
+
+-- Obtiene la pizza con menor precio de una lista
+getMenorPrecio :: [Pizza] -> Pizza
+getMenorPrecio (cab:[]) = cab
+getMenorPrecio (cab:(cab2:rest))
+	| precio(cab) <= precio(cab2) = getMenorPrecio ([cab]++rest)
+	| precio(cab2) < precio(cab) = getMenorPrecio ([cab2]++rest)
+
+-- Obtiene el segmento que comienza con la pizza que tiene menor precio superior a X euros.
+-- Llama a la función para que acepte un parámetro tipo lista de pizzas.
+dropPrecio :: Precio -> [Pizza]
+dropPrecio x = dropPrecioRec x listaPizzas
+
+-- Obtiene el segmento que comienza con la pizza que tiene menor precio superior a X euros.
+dropPrecioRec :: Precio -> [Pizza] -> [Pizza]
+dropPrecioRec _ [] = []
+dropPrecioRec x (cab:rest) 
+	| cab==getMenorPrecio(precioSuperiorA x ([cab]++rest)) = [cab]++rest
+	| otherwise = dropPrecioRec x rest
