f4a7bd57892a856334b7736c246fb338654ef0b1
albertofermer <albertofernmer@gmail.com>
2022-12-30 11:21:05 +0100

    auto-commit:30/12/2022 a las 11:21:05,33

--- 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Trabajo_SegmentacionReconocimientoCaracteresMatriculas/00_SegmentacionMatriculas/Funciones_Necesarias/filtro_orden_percentil.m
+++ 1º Cuatrimestre/[VC] Visión por Computador/Prácticas/Trabajo_SegmentacionReconocimientoCaracteresMatriculas/00_SegmentacionMatriculas/Funciones_Necesarias/filtro_orden_percentil.m
@@ -18,22 +18,12 @@ end
 if(strcmp('zeros',opcion))
     % Amplía la imagen con ceros
     Iamp = zeros(NH+N,MH+M);
-    Iamp = uint8(Iamp);
     % Incrustamos la imagen en la imagen ampliada.
     Iamp(1+EC:(N+EC), 1+EF:(M+EF)) = I;
 
 else
     Iamp = padarray(I, [EF, EC], opcion);    
 end
-
-%-------------------------------------------------------------------------%
-% Para aplicar la máscara H a la imagen se recorrerá la imagen ampliada y
-% se hará la operación de convolución para cada píxel.
-%
-% ROI: entorno de vecindad del píxel.
-% conv: matriz multiplicada por la máscara.
-% ImagenNueva: La imagen con el píxel actualizado.
-%-------------------------------------------------------------------------%
 
 for k=(1+EF):(N+EF)
     for j=(1+EC):(M+EC)
