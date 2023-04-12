d2fe60465e1bf24e0262855d916162add412e304
albertofermer <albertofernmer@gmail.com>
2023-04-12 12:29:56 +0200

    auto-commit:12/04/2023 a las 12:29:56,52

--- 2º Cuatrimestre/[MByHB] Modelos Bioinspirados y Heurísticas de Búsquedas/Prácticas/Práctica 2. Modelos Bioinspirados/PSO_GLOBAL/Global_PSO.py
+++ 2º Cuatrimestre/[MByHB] Modelos Bioinspirados y Heurísticas de Búsquedas/Prácticas/Práctica 2. Modelos Bioinspirados/PSO_GLOBAL/Global_PSO.py
@@ -1,47 +1,57 @@
 import numpy as np
 import Utils
 
-
+'''
+Particle Swarm Optimization con vecindad global.
+'''
 def Global_PSO(semilla, funcion, vel, iteraciones_sin_mejora):
-    np.random.seed(semilla)
-    idfun = 0 if funcion == Utils.RosenbrockFunction else 1
-    t = 0
-    # Inicializar X y V (Valores aleatorios entre MAX y MIN)
-    if funcion == Utils.RosenbrockFunction:
+    np.random.seed(semilla) # Inicializamos la semilla
+    idfun = 0 if funcion == Utils.RosenbrockFunction else 1     # Obtenemos el identificador de cada funcion de evaluación
+    t = 0   # Numero de iteraciones a 0.
+    # Inicializar X y V (Valores aleatorios entre MAX y MIN). Cada función tiene límites diferentes.
+    if idfun == 0:  # Rosenbrock
         _x = (np.random.uniform(Utils.MIN_POS_ROS_X, Utils.MAX_POS_ROS_X, Utils.NUM_PARTICLES))
         y = (np.random.uniform(Utils.MIN_POS_ROS_Y, Utils.MAX_POS_ROS_Y, Utils.NUM_PARTICLES))
         x = np.column_stack((_x, y))
-    else:
+    else:   # Rastrigin
         x = np.random.uniform(Utils.MIN_POS_RAS, Utils.MAX_POS_RAS, (Utils.NUM_PARTICLES, Utils.DIMENSION))
-
+    # La velocidad es aleatoria entre -VEL_MAX y +VEL_MAX
     v = np.random.uniform(vel[0], vel[1], (Utils.NUM_PARTICLES, Utils.DIMENSION))
     pbest = x.copy()  # Al principio, cada particula tiene como mejor posición ella misma.
+    # Se inicializa el numero de evaluaciones a la longitud de pbest porque se aplica la funcion
+    # de evaluación a cada particula para generar best_valores.
     num_evaluaciones = len(pbest)
     best_valores = np.apply_along_axis(funcion, 1, pbest)  # Aplica la funcion por filas a la matriz.
-    gbest = [10, 10]
-    mejor_valor = float("inf")
-    mejor_posicion = np.ones(Utils.DIMENSION)
+    gbest = [10, 10] # gbest arbitrario, puede ser cualquiera que no sea el óptimo.
+    mejor_valor = float("inf")  # El mejor valor se inicializa al máximo
+    mejor_posicion = np.ones(Utils.DIMENSION) # Da igual a qué se inicalice porque no se usa en las comparaciones.
 
-    # Graficas
+    # Listas para generar las graficas
     mejores_posiciones = []
     mejores_valores = []
     coordenadas = x.copy()
+    # Mientras no se cumpla la condición (numero de iteraciones sin mejorar la solución)
     while t < iteraciones_sin_mejora:
         for i in range(Utils.NUM_PARTICLES):
             num_evaluaciones += 1
-            valor = funcion(x[i, :])  # Evaluar X_i
+            # Evalúo la partícula i
+            valor = funcion(x[i, :])
             num_evaluaciones += 1
+            # Obtengo el valor de la mejor posición de la partícula i
             pbest_valor = funcion(pbest[i, :])
-            if valor < pbest_valor:  # Encontrar el mínimo
+            # Si es mejor, actualizamos la posición
+            if valor < pbest_valor:
                 pbest[i, :] = x[i, :].copy()
                 best_valores[i] = valor
                 num_evaluaciones += 1
                 pbest_valor = funcion(pbest[i, :])
 
             num_evaluaciones += 1
+            # La mejor posición global será la posición de la partícula que mejor fitness tenga.
             if pbest_valor < funcion(gbest):
                 gbest = pbest[i, :].copy()
 
+            # Actualizamos el mejor valor de la partícula
             if pbest_valor < mejor_valor:
                 t = 0
                 mejor_valor = pbest_valor
@@ -49,6 +59,7 @@ def Global_PSO(semilla, funcion, vel, iteraciones_sin_mejora):
                 # print(mejor_valor)
                 # print(mejor_posicion)
 
+        # Para cada partícula:
         for i in range(Utils.NUM_PARTICLES):
             # Calcular v[i] de acuerdo a pbest[i] y gbest[i]
             # Calcular nueva posicion x[i]
--- 2º Cuatrimestre/[MByHB] Modelos Bioinspirados y Heurísticas de Búsquedas/Prácticas/Práctica 2. Modelos Bioinspirados/Utils.py
+++ 2º Cuatrimestre/[MByHB] Modelos Bioinspirados y Heurísticas de Búsquedas/Prácticas/Práctica 2. Modelos Bioinspirados/Utils.py
@@ -51,6 +51,7 @@ def RastriginFunction(x):
 
 
 def RepresentarFuncion(funcion, ax, semilla, iteracion):
+    # Cada función de evaluación tiene unos límites diferentes.
     if funcion == RosenbrockFunction:
         x = np.linspace(MIN_POS_ROS_X, MAX_POS_ROS_X)
         y = np.linspace(MIN_POS_ROS_Y, MAX_POS_ROS_Y)
@@ -58,12 +59,17 @@ def RepresentarFuncion(funcion, ax, semilla, iteracion):
         x = np.linspace(MIN_POS_RAS, MAX_POS_RAS)
         y = np.linspace(MIN_POS_RAS, MAX_POS_RAS)
 
+    # Generamos el espacio de la función
     X1, X2 = np.meshgrid(x, y)
+    # Añadimos el título de la gráfica
     plt.title(f"{'Función Rosenbrock' if funcion == RosenbrockFunction else 'Funcion Rastrigin'}\n"
               f"Semilla: {semilla}\n"
               f"Iteración: {iteracion}\n")
+    # Graficamos la superficie de la gráfica
     ax.plot_surface(X1, X2, funcion([X1, X2]), cmap='jet', alpha=0.5)
+    # Le damos orientación para verla desde arriba
     ax.view_init(elev=90, azim=180)
+    # Añadimos las etiquetas a los ejes
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel(f'{"ROS" if funcion == RosenbrockFunction else "RAS"}(X, Y)')
@@ -75,22 +81,25 @@ Calcula la siguiente posición de una partícula.
 
 
 def GetNextPositionAndSpeed(omega, v, phi_1, pbest, phi_2, g, pos, vel, function):
+    # Inicializamos los vectores de la siguiente posición y la siguiente velocidad.
     next_pos = np.zeros(len(pos))
     next_v = np.zeros(len(pos))
-    for i in range(len(pos)):  # Para cada dimension del vector posicion,
+    # Para cada dimensión del vector posición
+    for i in range(len(pos)):
+        # Calculamos la siguiente velocidad aplicando la fórmula
         next_v[i] = omega * v[i] + phi_1 * np.random.random() * (pbest[i] - pos[i]) + phi_2 * np.random.random() * (
                 g[i] - pos[i])
-
+        # Controlamos que la componente de cada velocidad no supere la velocidad máxima
         if next_v[i] >= vel[1]:
             next_v[i] = vel[1]
         elif next_v[i] <= vel[0]:
             next_v[i] = vel[0]
 
+        # La siguiente posición será la suma
         next_pos[i] = pos[i] + next_v[i]
 
-        # Si se sale de los límites del tablero
+        # Controlamos que no se salga de los límites de las gráficas
         if function == 0:  # Rosenbrock
-
             if next_pos[i] >= MAX_POS_ROS_X and i == 0:
                 next_pos[i] = MAX_POS_ROS_X
             elif next_pos[i] <= MIN_POS_ROS_X and i == 0:
@@ -111,34 +120,55 @@ def GetNextPositionAndSpeed(omega, v, phi_1, pbest, phi_2, g, pos, vel, function
 
 
 def GetEntorno(indice_particula):
+    # Obtenemos el entorno de cada partícula obteniendo el módulo del índice de la partícula:
+    # i - 2 % NUM_PARTICULAS
+    # i - 1 % NUM_PARTICULAS
+    # i + 1 % NUM_PARTICULAS
+    # i + 2 % NUM_PARTICULAS
     return [(indice_particula + i) % NUM_PARTICLES for i in range(-VECINDAD, VECINDAD + 1) if i != 0]
 
-
+'''
+Esta función se llama a la hora de realizar la animación del movimiento de las partículas en 
+FuncAnimation. Se ejecuta una vez por cada frame que se especifique y se encarga de dibujar
+cada partícula sobre la función de evaluación.
+'''
 def func(i, lista, funcion, semilla):
-    len(lista)
     coord = np.zeros(shape=(NUM_PARTICLES, 2), dtype=float)
-    # Las 10 primeras posiciones
+
+    # Recorremos la matriz lista de 10 en 10 posiciones. En cada
+    # frame se recorren las 10 partículas para ir generando
+    # la animación
     for j in range(0 + (NUM_PARTICLES * i), NUM_PARTICLES * (i + 1)):
-        print(len(lista) - j)
-        # print(len(lista))
-        if j < len(lista):
+        print(len(lista) - j) # Contador para saber cuánto le queda a la animación para realizarse.
+        if j < len(lista): # Si se supera la longitud de la lista no hace nada (para evitar errores)
             coord[j % NUM_PARTICLES, 0] = lista[j, 0]
             coord[j % NUM_PARTICLES, 1] = lista[j, 1]
-
+    # Limpiamos la gráfica
     plt.cla()
+    # Generamos la gráfica con la función de evaluación
     RepresentarFuncion(funcion, ax, semilla, int(i))
+    # Pintamos las partículas correspondientes al frame i.
     ax.scatter(coord[:, 0], coord[:, 1], funcion([coord[:, 0], coord[:, 1]]), s=10, c="black", alpha=1)
 
-
+'''
+Función para generar la animación de las partículas en la función pasada por parámetro.
+'''
 def Dibujar(fun, x, nombre, semilla):
+    # Generamos la figura
     fig = plt.figure(figsize=[12, 8])
+    # Inicializamos el eje como proyección 3D y lo hacemos una variable global.
     global ax
     ax = plt.axes(projection='3d')
+    # Empezamos la animación
     ani = FuncAnimation(fig, partial(func, lista=x, funcion=fun, semilla=semilla), frames=int(len(x) / 10), interval=1,
                         blit=False)
+    # Guardamos la animación.
     ani.save(nombre, fps=15)
 
 
+'''
+Función que genera los vídeos de la animación de cada función para todas las semillas.
+'''
 def generarVideos(pso):
     for f in [RosenbrockFunction, RastriginFunction]:
         for s in range(len(SEEDS)):
@@ -152,7 +182,10 @@ def generarVideos(pso):
                 x = pso(SEEDS[s], f, (-0.25, 0.25), 400)[5]
             Dibujar(f, x, f".\\Videos\\{'ROS' if f == RosenbrockFunction else 'RAS'}_S{SEEDS[s]}.mp4", SEEDS[s])
 
-
+'''
+Función que realiza una experimentación de algunos parámetros para encontrar los mejores
+para cada situación.
+'''
 def experimentacion(pso):
     mejores_parametros_ros = np.array([(-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1)], dtype=float)
     mejores_parametros_ras = np.array([(-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1)], dtype=float)
@@ -172,11 +205,15 @@ def experimentacion(pso):
     return mejores_parametros_ros, mejores_parametros_ras
 
 
+'''
+Función para generar las gráficas de fitness de los algoritmos de PSO.
+'''
 def graficas_resultados(pso):
-    for f in [RosenbrockFunction, RastriginFunction]:
-        evaluaciones = np.zeros(len(SEEDS))
-        mejores_valores = np.zeros(len(SEEDS))
-        for s in range(len(SEEDS)):
+    for f in [RosenbrockFunction, RastriginFunction]:   # Para cada función
+        evaluaciones = np.zeros(len(SEEDS)) # Inicializamos la lista de evaluaciones a 0
+        mejores_valores = np.zeros(len(SEEDS))  # Inicializamos la lista de evaluaciones a 0
+        for s in range(len(SEEDS)): # Para cada semilla
+            # Dependiendo de la función y del algoritmo, es mejor utilizar unos parámetros u otros.
             if pso.__name__ == PSO_GLOBAL.Global_PSO.Global_PSO.__name__ and f == RosenbrockFunction:
                 x = pso(SEEDS[s], f, (-0.2, 0.2), 400)
             elif pso.__name__ == PSO_GLOBAL.Global_PSO.Global_PSO.__name__ and f == RastriginFunction:
@@ -186,9 +223,10 @@ def graficas_resultados(pso):
             else: # pso == PSO_LOCAL.Local_PSO.Local_PSO and f == RastriginFunction:
                 x = pso(SEEDS[s], f, (-0.25, 0.25), 400)
 
+            # Guardamos los mejores valores y las evaluaciones
             mejores_valores[s] = x[1]
             evaluaciones[s] = x[4]
-            # print(f"Semilla: {SEEDS[s]}, Funcion: {f} = {x[0:2]}")
+            # Dibujamos las gráficas de fitness:
             fig, ax = plt.subplots()
             ax.plot([i for i in range(len(x[3]))], x[3],
                     label=f"{'Rosenbrock(x,y)' if f == RosenbrockFunction else 'Rastrigin(x,y)'}")
@@ -209,6 +247,8 @@ def graficas_resultados(pso):
         pd.set_option('display.max_columns', None)
         pd.set_option('display.width', None)
         pd.set_option('display.max_colwidth', None)
+
+        # Tabla de las métricas de cada algoritmo.
         data = {
             'Media Evaluaciones': [statistics.mean(evaluaciones)],
             'Mejor Evaluación': [min(evaluaciones)],
@@ -217,6 +257,7 @@ def graficas_resultados(pso):
             'Mejor Valor': [min(mejores_valores)],
             'Desviación Valor': [statistics.stdev(mejores_valores)]
         }
+
         print(pd.DataFrame(data))
 
 
